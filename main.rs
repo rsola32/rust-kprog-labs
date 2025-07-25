53766a4f15d249f1fa30b8322babe8cef27c8a95
Rajesh Sola <rsola@gitam.edu>
2025-07-25 11:47:08 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,14 +1,13 @@
 #![allow(missing_docs)]
 
 use kernel::prelude::*;
-use kernel::{str::CStr, ThisModule};
 
 module! {
     type: HelloWorld,
-    name: b"hello_world",
-    author: b"Douglas Su",
-    description: b"A simple hello world example",
-    license: b"GPL v2",
+    name: "hello_world",
+    author: "Rajesh Sola.",
+    description: "A simple hello world example",
+    license: "GPL v2",
 }
 
 struct HelloWorld;
